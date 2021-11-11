6f64383ce1b86ba38a23eda05849bd7696d1ec32
courtney-bawuah <courtney.bawuah@cloudreach.com>
2021-11-11 12:08:50 +0000

    python basics

---
+++ loops.py
@@ -0,0 +1,19 @@
+#!/Users/courtney.bawuah/.pyenv/shims/python
+
+planets = ["Mercury", "Venus", "Earth", "Mars"]
+
+print(planets)
+
+planets.append("Jupter")
+
+print(planets)
+
+planets.insert(3, "Moon")
+
+print(planets)
+
+planets.remove("Moon")
+
+print(planets)
+
+print("Mars" in planets)
